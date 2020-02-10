045914179b4e4a265007d13b80582115c31157c9
Piotr <piotrkiljanczyk@outlook.com>
2020-02-10 14:50:57 +0100

    Author: Piotr Kiljańczyk Description: Added comment

--- src/test/java/FirstAutomatedTest.java
+++ src/test/java/FirstAutomatedTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
+/*
+@author: Piotr Kiljanczyk
+ */
 public class FirstAutomatedTest {
 
     private WebDriver driver;
